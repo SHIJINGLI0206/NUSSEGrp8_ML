331946848cfcacdf701232ce3d5900deec04207f
Angsnk <e0146462@u.nus.edu>
2017-09-21 18:12:51 +0800

    Add files via upload
    Added scaling feature and loading of data

--- DataManagement/dataManager.py
+++ DataManagement/dataManager.py
@@ -3,6 +3,9 @@
 # feature scaling, manipulation, reshaping etc          #
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Normalizer
 
 class dataManager:
 
@@ -12,20 +15,41 @@ class dataManager:
     def setFormat(self, format):
         self._format = format
 
-    def loadData(self, fileName, outputTag, percentTrain):
+    def loadData(self, fileName, listOfLabelsToDrop, outputTag, percentTrain):
         self.fileName = fileName
         if self._format == 'csv':
             # load Dataset
             self.dataFrame = pd.read_csv(self.fileName)
-            self.inputData = pd.read_csv(self.fileName, usecols=lambda x: x not in [outputTag])
-            self.outputData = pd.read_csv(self.fileName, usecols=[outputTag])
+            temp = self.dataFrame.copy()
+            #self.inputData = pd.read_csv(self.fileName, usecols=lambda x: x not in [outputTag])
+            temp.drop(listOfLabelsToDrop, axis=1, inplace=True)
+            self.outputData = temp[outputTag]
+            #print self.outputData
+
+            temp.drop(outputTag, axis=1, inplace=True)
+            self.inputData = temp
+            #print self.inputData
+
 
             # Split into training and testing dataset
+            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.inputData, self.outputData, train_size=percentTrain)
 
         elif self._format == 'json':
             print 'JSON format not supported'
         else:
             print 'No format selected'
+
+    def standardizeScaling(self):
+        self.scaler = StandardScaler().fit(self.X_train)  #scaler to be reapply on testing set later
+        #print 'Mean\n'
+        #print self.scaler.mean_
+        #print 'Standard deviation\n'
+        #print self.scaler.var_
+        self.X_trainScaled = self.scaler.transform(self.X_train)
+
+    def normalize(self):
+        self.scaler = Normalizer().fit(self.X_train)  #scaler to be reapply on testing set later
+        self.X_trainScaled = self.scaler.transform(self.X_train)
 
 
 

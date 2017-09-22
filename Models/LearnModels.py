f509dc495b8739346b7f5bed7ada723c0ffb0836
Angsnk <e0146462@u.nus.edu>
2017-09-22 16:27:07 +0800

    Add files via upload
    Add Gradient Descent model

--- Models/LearnModels.py
+++ Models/LearnModels.py
@@ -9,25 +9,26 @@ class LinearRegModel:
 
     def predict(self, X, yTrue):
         yPredicted = self.regr.predict(X)
-        RMSE = mean_squared_error(yTrue, yPredicted)
+        RMSE = mean_squared_error(yTrue, yPredicted)**0.5
         return yPredicted, RMSE, self.regr.intercept_, self.regr.coef_
 
     def score(self, X, y):
         return self.regr.score(X, y.as_matrix())
 
-class AnotherLinearRegModel:
+class GradientDescentModel:
 
-    def __init__(self, X, y):
-        self.regr = linear_model.LinearRegression()
-        self.regr.fit(X,y)
+    def __init__(self, X, y, coef_init=None, intercept_init=None):
+        self.regr = linear_model.SGDRegressor(n_iter=1000, eta0=0.003)
+        self.regr.fit(X,y, coef_init, intercept_init)
 
     def predict(self, X):
-        return self.regr.predict(X) #return predicted values
+        #return predicted values
+        return self.regr.predict(X)
 
     def score(self, X, y):
         return self.regr.score(X, y)
 
     def toString(self):
-        print self.regr.get_params()
+        print self.regr.n_iter
         print self.regr.coef_
         print self.regr.intercept_
